6478cf4a802d8be595a2275b163611dfe1b7357a
Emil Hultcrantz <emilhul@kth.se>
2021-09-28 14:27:33 +0200

    Adds initialization of pawn attack tables and helper funciton for moving one step in any direction

--- src/lib.rs
+++ src/lib.rs
@@ -27,27 +27,66 @@ pub struct Game {
     bishop: u64,
     rook: u64,
     queen: u64,
-    king: u64
+    king: u64, 
+    empty: u64,
     
+    // Array move table
+    white_pawn_attacks: [u64; 64],
+    black_pawn_attacks: [u64; 64],
 }
 
 impl Game {
-    /// Initialises a new board with pieces.
+    /// Initializes a new board with pieces.
     pub fn new() -> Game {
-        Game {
-            /* initialise board, set active colour to white, ... */
+        let mut game = Game {
+            /* initialize board, set active colour to white, ... */
             state: GameState::InProgress,
 
-            // Inititalise bitboards. LSF bit first
-            white:  0b11111111_11111111_00000000_00000000_00000000_00000000_00000000_00000000,  
-            black:  0b00000000_00000000_00000000_00000000_00000000_00000000_11111111_11111111,
+            // Inititalise bitboards. LSF bit last
+            white:  0b00000000_00000000_00000000_00000000_00000000_00000000_11111111_11111111,
+            black:  0b11111111_11111111_00000000_00000000_00000000_00000000_00000000_00000000,  
             pawn:   0b00000000_11111111_00000000_00000000_00000000_00000000_11111111_00000000,
             knight: 0b01000010_00000000_00000000_00000000_00000000_00000000_00000000_01000010,
             bishop: 0b00100100_00000000_00000000_00000000_00000000_00000000_00000000_00100100,
             rook:   0b10000001_00000000_00000000_00000000_00000000_00000000_00000000_10000001,
-            queen:  0b00010000_00000000_00000000_00000000_00000000_00000000_00000000_00010000,
-            king:   0b00001000_00000000_00000000_00000000_00000000_00000000_00000000_00001000,
+            queen:  0b00001000_00000000_00000000_00000000_00000000_00000000_00000000_00001000,
+            king:   0b00010000_00000000_00000000_00000000_00000000_00000000_00000000_00010000,
+            empty:  0b00000000_00000000_11111111_11111111_11111111_11111111_00000000_00000000,
+
+            white_pawn_attacks: [0; 64],
+            black_pawn_attacks: [0; 64],
+        };
+        game
+    }
+
+    pub fn one_step(dir: &str, bit: u64) -> u64 {
+        let not_a_file: u64 = 0b11111110_11111110_11111110_11111110_11111110_11111110_11111110_11111110;
+        let not_h_file: u64 = 0b01111111_01111111_01111111_01111111_01111111_01111111_01111111_01111111;
+        match dir {
+            "north" => bit<<8,
+            "south" => bit>>8,
+            "east" => (bit<<1)&not_a_file,
+            "no_ea" => (bit<<9)&not_a_file,
+            "so_ea" => (bit>>7)&not_a_file,
+            "west" => (bit>>1)&not_h_file,
+            "no_we" => (bit<<7)&not_h_file,
+            "so_we" => (bit>>9)&not_h_file,
+            _ => bit
         }
+    }
+
+    fn initialize_move_table(&mut self) {
+        let mut square_bit: u64 = 0b10000000_00000000_00000000_00000000_00000000_00000000_00000000_00000000;
+        for i in 0..64 { 
+            self.white_pawn_attacks[i] = Game::one_step("no_ea", square_bit)|Game::one_step("no_we", square_bit);
+            square_bit>>=1;
+        }
+        square_bit = 0b10000000_00000000_00000000_00000000_00000000_00000000_00000000_00000000;
+        for i in 0..64 { 
+            self.black_pawn_attacks[i] = Game::one_step("so_ea", square_bit)|Game::one_step("so_we", square_bit);
+            square_bit>>=1;
+        }
+
     }
 
     /// If the current game state is InProgress and the move is legal, 
@@ -80,8 +119,6 @@ impl Game {
 /// Output example:
 /// rnbqkbnr
 /// pppppppp
-/// ********
-/// ********
 /// ********
 /// ********
 /// ********
@@ -165,7 +202,7 @@ impl fmt::Debug for Game {
                 // 7-rank is added to reverse each rank. To display it as we'd usually represent the board.
                 // Since we use lsf but loop over the bit 0..63 we add the ranks in reverse order,
                 // Creating a mirrored board.
-                board_string.push(board[file][7-rank]);
+                board_string.push(board[7-file][rank]);
             }
         }
         write!(f, "{}", board_string)
@@ -188,14 +225,59 @@ mod tests {
     }
 
     // example test
-    // check that game state is in progress after initialisation
+    // check that game state is in progress after initialization
     #[test]
-    fn game_in_progress_after_init() {
-
-        let game = Game::new();
-
+    fn game_init() {
+        println!();
+        print!("1... ");
+        let mut game = Game::new();
         println!("{:?}", game);
-
         assert_eq!(game.get_game_state(), GameState::InProgress);
+        println!("ok");
+
+        print!("2... ");
+        game.initialize_move_table();
+        assert_eq!(game.white_pawn_attacks[0], 0);
+        println!("ok");
+        print!("3... ");
+        assert_eq!(game.white_pawn_attacks[9], 0b10100000_00000000_00000000_00000000_00000000_00000000_00000000_00000000);
+        println!("ok");
+    }
+
+    #[test]
+    fn shift_test() {
+        println!();
+        print!("1... ");
+        let bit: u64 =                              0b00000000_00000000_00000000_10000000_00000000_00000000_00000000_00000000;
+        assert_eq!(Game::one_step("north", bit),    0b00000000_00000000_10000000_00000000_00000000_00000000_00000000_00000000);
+        println!("ok");
+        print!("2... ");
+        let bit: u64 =                              0b00000000_00000000_00000000_10000000_00000000_00000000_00000000_00000000;
+        assert_eq!(Game::one_step("south", bit),    0b00000000_00000000_00000000_00000000_10000000_00000000_00000000_00000000);
+        println!("ok");
+        print!("3... ");
+        let bit: u64 =                              0b00000000_00000000_00000000_10000000_00000000_00000000_00000000_00000000;
+        assert_eq!(Game::one_step("east", bit),     0b00000000_00000000_00000000_00000000_00000000_00000000_00000000_00000000);
+        println!("ok");
+        print!("4... ");
+        let bit: u64 =                              0b00000000_00000000_00000000_01000000_00000000_00000000_00000000_00000000;
+        assert_eq!(Game::one_step("west", bit),     0b00000000_00000000_00000000_00100000_00000000_00000000_00000000_00000000);
+        println!("ok");
+        print!("5... ");
+        let bit: u64 =                              0b00000000_00000000_00000000_01000000_00000000_00000000_00000000_00000000;
+        assert_eq!(Game::one_step("no_ea", bit),    0b00000000_00000000_10000000_00000000_00000000_00000000_00000000_00000000);
+        println!("ok");
+        print!("6... ");
+        let bit: u64 =                              0b00000000_00000000_00100000_00000000_00000000_00000000_00000000_00000000;
+        assert_eq!(Game::one_step("no_we", bit),    0b00000000_00010000_00000000_00000000_00000000_00000000_00000000_00000000);
+        println!("ok");
+        print!("7... ");
+        let bit: u64 =                              0b00000000_00000000_01000000_00000000_00000000_00000000_00000000_00000000;
+        assert_eq!(Game::one_step("so_ea", bit),    0b00000000_00000000_00000000_10000000_00000000_00000000_00000000_00000000);
+        println!("ok");
+        print!("8... ");
+        let bit: u64 =                              0b00000000_00000000_00000000_00000000_00100000_00000000_00000000_00000000;
+        assert_eq!(Game::one_step("so_we", bit),    0b00000000_00000000_00000000_00000000_00000000_00010000_00000000_00000000);
+        println!("ok");
     }
 }
